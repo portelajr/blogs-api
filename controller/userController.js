8d7c42e6ed6d379677cb6fbc2e7c38abbf393143
Junior Portela <portelajr14@gmail.com>
2021-12-06 01:17:00 -0300

    database criado com variaveis ambiente e migration create-users feita

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,5 @@
-const createUser = async (req, res) => {
-
+const createUser = async (_req, _res) => {
+// modficiar parametros
 };
+
+module.exports = { createUser };
---
+++ models/user.js
@@ -0,0 +1,10 @@
+// const User = (sequelize, DataTypes) => {
+//   const user = sequelize.define('User', {
+//     fullName: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//   });
+
+//   return user;
+// };
+
+// module.exports = User;
