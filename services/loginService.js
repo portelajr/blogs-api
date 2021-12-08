f0be39e69c66d991b1e34a74556588319cb8d813
Junior Portela <portelajr14@gmail.com>
2021-12-08 15:01:19 -0300

    fix: remove console.log gerando erro em serviceLogin

--- services/loginService.js
+++ services/loginService.js
@@ -3,21 +3,16 @@ const loginEntries = require('../utils/loginValidations');
 const { User } = require('../models/index');
 
 const loginUser = async (payload) => {
-  try {
-    await loginEntries(payload);
-    const { email } = payload;
-    const user = await User.findOne({ where: { email } });
-    console.log(user.dataValues);
-    if (user === null) {
-      const err = { code: 400, message: 'Invalid fields' };
-      throw err;
-    }
-
-    return tokenGenerator(user);
-  } catch (error) {
-    const err = { ...error };
+  loginEntries(payload);
+  const { email } = payload;
+  const user = await User.findOne({ where: { email } });
+    
+  if (user === null) {
+    const err = { code: 400, message: 'Invalid fields' };
     throw err;
   }
+
+  return tokenGenerator(user);
 };
 
 module.exports = { loginUser };
